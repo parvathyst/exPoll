1d8db7356f58de3b1e12c19fdbf943000dc1db0a
Bindhiya C Philip <bindhiya.philip@experionglobal.local>
2024-10-25 09:30:13 +0530

    feat: added generate link funtionality

---
+++ adminCreatePoll/generatelink.js
@@ -0,0 +1,36 @@
+const baseUrl = 'https://www.example.com'; 
+const generateButton = document.getElementById('generate-button');
+const linkContainer = document.getElementById('show');
+let uniqueLink = ''; // Declare the variable in the outer scope
+
+
+generateButton.addEventListener('click', () => {
+    // const uniqueId = generateUniqueId();
+    const uniqueId = 2;
+    uniqueLink = `${baseUrl}?id=${uniqueId}`;
+
+    const linkElement = document.createElement('div');
+    linkElement.classList.add('link-box'); // Add the CSS class
+    linkElement.innerHTML =`
+        <a href="${uniqueLink}" target="_blank" style="color:white">${uniqueLink}</a>
+    `;
+    // linkContainer.value = linkElement
+    
+    linkContainer.appendChild(linkElement);
+});
+
+// function generateUniqueId() {
+//     return Math.random().toString(36).substr(2,9); // Generates a random unique ID
+// }
+
+const copyButton = document.getElementById('copy-button');
+
+copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(uniqueLink)
+        .then(() => {
+            alert('Link copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+});
