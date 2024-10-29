3ddcc38db540b74a1f64c375f1c70525fdbe5728
Arya V S <aryavalsala2018@gmail.com>
2024-10-29 21:11:15 +0530

    update : added admin dashboard activity section

--- src/pages/Admin/createPoll/adminCreatePoll.js
+++ src/pages/Admin/createPoll/adminCreatePoll.js
@@ -12,12 +12,12 @@ function addOption(value) {
     if (value === undefined) {
         optionContainer.innerHTML = `
         <input type="text" placeholder="Enter Option"  class="D-no-border">
-        <img src="icons/trash_icon.svg" alt="delete icon" onclick="removeOption(this)" />
+        <img src="/exPoll/src/assets/icons/trash.png" alt="delete icon" onclick="removeOption(this)" />
         `;
     } else {
         optionContainer.innerHTML = `
         <input type="text" placeholder="Enter Option" value="${value}" class="D-no-border">
-        <img src="icons/trash_icon.svg" alt="delete icon" onclick="removeOption(this)" />
+        <img src="/exPoll/src/assets/icons/trash.png" alt="delete icon" onclick="removeOption(this)" />
         `;
     }
     document.querySelector('.options-container-none').insertAdjacentElement('afterend', optionContainer);
