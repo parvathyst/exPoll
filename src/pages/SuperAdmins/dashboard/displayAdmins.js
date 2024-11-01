f202234ca305cb4c2589e002e99e69ee569a4cce
diuuuuuu <diyoof.exp@gmail.com>
2024-11-01 08:35:59 +0530

    feat: create poll - generate link button

--- src/pages/SuperAdmins/dashboard/displayAdmins.js
+++ src/pages/SuperAdmins/dashboard/displayAdmins.js
@@ -1,7 +1,7 @@
 import { getAdmins } from "../../../backend/firebase/superAdmin/getAdmins.js"
 
 
-async function displayAdmins() {
+async function displayAdmins(callback) {
     const value = await getAdmins();
     document.getElementById("super-admin-container-bottom").innerHTML = `<div class="admin-items-none"></div>`;
     Object.values(value).forEach(data => {
@@ -9,15 +9,17 @@ async function displayAdmins() {
         adminItem.classList.add('admin-item');
         adminItem.innerHTML = `
         <div class="admin-details">
-                    <strong>${data.fullName}</strong><br>
-                    <span>${data.email}</span>
+                    <strong class = "admin-name">${data.fullName}</strong><br>
+                    <span class="admin-email">${data.email}</span>
                 </div>
                 <div class="action-buttons">
                 <button class="view"><img src="../../../assets/icons/pen.png" alt="edit icon"></button>
-                <button class="delete"><img src="../../../assets/icons/trash.png" alt="delete icon"></button>
+                <button class="delete-admin-button"><img src="../../../assets/icons/trash.png" alt="delete icon"></button>
                 </div>
                 `;
         document.querySelector('.super-admin-container-bottom').insertBefore(adminItem, document.querySelector('.admin-items-none'));
     });
+
+    if (callback) callback();
 }
 export { displayAdmins }
