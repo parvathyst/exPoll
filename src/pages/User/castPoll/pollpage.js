d3b4ed955e72158e481eea9227b8b7658cd52e0c
Bindhiya C Philip <bindhiya.philip@experionglobal.local>
2024-11-11 19:22:01 +0530

    feat:cast poll

--- src/pages/User/castPoll/pollpage.js
+++ src/pages/User/castPoll/pollpage.js
@@ -169,7 +169,7 @@ function displayOption(pollOption) {
     pollItem.classList.add("unlocked-poll-card");
   } else {
     pollItem.innerHTML = `
-    <h4>${pollOption.content}</h4><img src="/src/assets/icons/lock.svg">
+    <h4>${pollOption.content}</h4><img src="/exPoll/src/assets/icons/lock.svg">
     `;
     pollItem.classList.add("locked-poll-card");
     pollItem.disabled = true;
